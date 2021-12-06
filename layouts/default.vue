d2af1b6921b586f1ff8abac9f27b055e3c6d44e2
qwertin1998@gmail.com <herejutep1998>
2021-12-06 03:06:21 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-form ref='form' disabled>
+      <v-text-field v-model='name' :rules='rules' label='Имя' style='max-width: 350px' required/>
+      <v-btn @click='validate' v-text='`Отправить`'/>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import {minLength,required} from "@/utils/validationRules"
+export default {
+  data: () => ({
+    name:"",
+  }),
+  computed:{
+    rules(){
+      const rules = []
+      rules.push(minLength,required)
+      return rules
+    }
+  },
+  methods: {
+    validate(){
+      console.log(this.$refs.form.validate())
+      this.$refs.form.validate();
+    }
+  }
+}
+</script>
